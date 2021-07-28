9c3c9f79ac1acb1bfd465490e00efde3fdc11c86
qfanfan <qfan@prosper.com>
2021-07-28 08:57:22 -0700

    add config center

--- edu-eureka-boot/src/main/java/com/learning/edu/EurekaServer.java
+++ edu-eureka-boot/src/main/java/com/learning/edu/EurekaServer.java
@@ -1,12 +1,12 @@
 package com.learning.edu;
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class LearningEurekaApplication {
+public class EurekaServer {
     public static void main(String[] args) {
-        SpringApplication.run(LearningEurekaApplication.class,args);
+        SpringApplication.run(EurekaServer.class,args);
     }
 }
